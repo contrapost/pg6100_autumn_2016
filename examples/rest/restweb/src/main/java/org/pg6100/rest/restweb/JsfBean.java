c5bd71ab4bfcb724c665eb5cdfaa28d4ab7a467e
arcuri82 <arcuri82@gmail.com>
2016-10-26 21:20:18 +0200

    fixed potential issue

--- examples/rest/restweb/src/main/java/org/pg6100/rest/restweb/JsfBean.java
+++ examples/rest/restweb/src/main/java/org/pg6100/rest/restweb/JsfBean.java
@@ -2,13 +2,14 @@ package org.pg6100.rest.restweb;
 
 
 import javax.ejb.EJB;
-import javax.faces.bean.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
 @Named
-@RequestScoped
-public class JsfBean {
+@SessionScoped
+public class JsfBean implements Serializable{
 
     @EJB
     private LocationProvider locationProvider;
